b45ae6bc51d1a9e4918f99fc083df1736a2cbbc5
camilo <camilo.rodriguezt@correo.usa.edu.co>
2021-10-15 18:53:19 -0500

    el controlador solo recibe datos por POST

--- webserver.py
+++ webserver.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=['get', 'post'])
 def hello_world():
-    #nombre = request.args.get('nombre')
-    #edad = request.args.get('edad')
     nombre = request.form.get('nombre')
     edad = request.form.get('edad')
     return f"<h1>Bienvenido {nombre}</h1><h2>Ud tiene {edad} años</h2>"
