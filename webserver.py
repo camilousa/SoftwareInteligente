3c4ba9ed2500a4b6052cbfef72e922d706e5e334
camilo <camilo.rodriguezt@correo.usa.edu.co>
2021-10-15 19:14:37 -0500

    se niega el acceso a menores de edad

--- webserver.py
+++ webserver.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/", methods=['get', 'post'])
 def hello_world():
     nombre = request.form.get('nombre')
-    edad = request.form.get('edad')
+    edad = int(request.form.get('edad'))
     return render_template("Bienvenido.html", nombre=nombre, edad=edad)
 
 
