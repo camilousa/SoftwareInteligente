d64cd6b4c3ea40b9df514686f6e86a48fc18cb3c
camilo <camilo.rodriguezt@correo.usa.edu.co>
2021-10-15 19:00:23 -0500

    se separó el código del controlador de la capa de la vista

--- webserver.py
+++ webserver.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def hello_world():
     nombre = request.form.get('nombre')
     edad = request.form.get('edad')
-    return f"<h1>Bienvenido {nombre}</h1><h2>Ud tiene {edad} años</h2>"
+    return render_template("Bienvenido.html", nombre=nombre, edad=edad)
 
 
 @app.route("/form") 
